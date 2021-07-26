a5d6e8412f5f18704bdf50014d7116d95272ee0e
Felix Almejo <fd.almejo@gmail.com>
2021-07-26 09:35:09 -0700

    Added hover over play and hover out pause functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("page loaded...");
+var video = document.getElementById("video-one")
+function videoPlay(element){
+    video.play();
+    element.volume = 0;
+}
+function videoPause(element){
+    video.pause();
+}
